244903765aa8e8453ab69efc8993125013dcb30c
Esteban Carvallo <crewxart@gmail.com>
2019-11-27 01:22:17 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,15 @@ Created on 26 nov. 2019
 
 @author: Ecarvallo
 '''
+
+class pythonMessage():
+    def __init__(self):
+        self.type="Text"        
+    def message(self, msg):
+        print(msg)
+        print(self.type)
+if __name__ == '__main__':
+    newMessage = pythonMessage()
+    newMessage.message("HelloWorld")
+    
+    
